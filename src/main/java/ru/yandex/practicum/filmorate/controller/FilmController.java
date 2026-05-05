6a0ef13fad76be36ce7c380d234d75a38de06321
User Asinenko <sinenkoanastasiia@yandex.com>
2026-05-05 16:13:13 +0300

    "add Bean Validation"

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,12 +1,12 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.Marker;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,8 +24,7 @@ public class FilmController {
     }
 
     @PostMapping
-    public Film create(@RequestBody Film film) {
-        validation(film);
+    public Film create(@Validated(Marker.Create.class) @RequestBody Film film) {
 
         film.setId(getNextId());
         films.put(film.getId(), film);
@@ -36,19 +35,31 @@ public class FilmController {
     }
 
     @PutMapping
-    public Film update(@RequestBody Film film) {
-        validation(film);
+    public Film update(@Validated(Marker.Update.class) @RequestBody Film film) {
 
         if (!films.containsKey(film.getId())) {
             log.warn("Такого фильма нет в базе с id = {}", film.getId());
             throw new NotFoundException("Фильм с id = " + film.getId() + " не найден");
         }
 
-        films.put(film.getId(), film);
+        Film oldFilm = films.get(film.getId());
 
-        log.info("Изменен фильм: {}", film);
+        if (film.getName() != null) {
+            oldFilm.setName(film.getName());
+        }
+        if (film.getDescription() != null) {
+            oldFilm.setDescription(film.getDescription());
+        }
+        if (film.getReleaseDate() != null) {
+            oldFilm.setReleaseDate(film.getReleaseDate());
+        }
+        if (film.getDuration() != null) {
+            oldFilm.setDuration(film.getDuration());
+        }
 
-        return film;
+        log.info("Изменен фильм: {}", oldFilm);
+
+        return oldFilm;
     }
 
     private int getNextId() {
@@ -60,24 +71,5 @@ public class FilmController {
         return ++currentMaxId;
     }
 
-    private void validation(Film film) {
-        if (film.getName() == null || film.getName().isBlank()) {
-            log.warn("Ошибка валидации фильма: пустое название");
-            throw new ValidationException("Название не может быть пустым");
-        }
-        if (film.getDescription() != null && film.getDescription().length() > 200) {
-            log.warn("Ошибка валидации фильма: описание длинее 200 символов");
-            throw new ValidationException("Максимальная длина описания — 200 символов");
-        }
-        if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            log.warn("Ошибка валидации фильма: не правильная дата релиза");
-            throw new ValidationException("Дата релиза — не раньше 28 декабря 1895 года");
-        }
-        if (film.getDuration() <= 0) {
-            log.warn("Ошибка валидации фильма: неположительная продолжительность");
-            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
-        }
 
-
-    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.Marker;
 
 import java.time.LocalDate;
 
@@ -9,9 +11,30 @@ import java.time.LocalDate;
  */
 @Data
 public class Film {
-    private int id;
+
+    private static final int MAX_DESCRIPTION_LENGTH = 200;
+    private static final LocalDate FIRST_RELEASE_DATE = LocalDate.of(1895, 12, 28);
+
+    @NotNull(groups = Marker.Update.class)
+    @Positive (groups = Marker.Update.class)
+    private Integer id;
+
+    @NotBlank (groups = Marker.Create.class)
     private String name;
+
+
+    @Size(max = MAX_DESCRIPTION_LENGTH, groups = {Marker.Create.class, Marker.Update.class})
     private String description;
+
+    @NotNull(groups = Marker.Create.class)
     private LocalDate releaseDate;
-    private int duration;
+
+    @NotNull(groups = Marker.Create.class)
+    @Positive(groups = {Marker.Create.class, Marker.Update.class})
+    private Integer duration;
+
+    @AssertTrue(groups = {Marker.Create.class, Marker.Update.class})
+    public boolean isReleaseDateValid() {
+        return releaseDate == null || !releaseDate.isBefore((FIRST_RELEASE_DATE));
+    }
 }
